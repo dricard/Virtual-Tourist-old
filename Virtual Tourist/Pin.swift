02c40c03012c458809e6a07758eba68c48ab13e9
Denis Ricard <d.ricard@me.com>
2016-05-02 09:33:42 -0400

    feat: basic structure of the project
    Add model classes Pin and Picture. Move the CoreData stack code out of AppDelegate and into its own class. Create a Model group and a Supporting Files group.

---
+++ Virtual Tourist/Pin.swift
@@ -0,0 +1,13 @@
+//
+//  Pin.swift
+//  Virtual Tourist
+//
+//  Created by Denis Ricard on 2016-05-02.
+//  Copyright © 2016 Hexaedre. All rights reserved.
+//
+
+import UIKit
+
+class Pin: NSObject {
+
+}
