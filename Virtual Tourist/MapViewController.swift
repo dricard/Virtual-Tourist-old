a3f7940c80452e441583f502308042f720dac195
Denis Ricard <d.ricard@me.com>
2016-05-02 15:24:47 -0400

    feat: add long press gesture recognizer to MapViewController

--- Virtual Tourist/MapViewController.swift
+++ Virtual Tourist/MapViewController.swift
@@ -21,17 +21,26 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let longPressRec = UILongPressGestureRecognizer()
+        longPressRec.addTarget(self, action: #selector(MapViewController.userLongPressed))
+        longPressRec.allowableMovement = 25
+        longPressRec.minimumPressDuration = 1.0
+        longPressRec.numberOfTouchesRequired = 1
+        view!.addGestureRecognizer(longPressRec)
+        
         mapView.delegate = self
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - MapView Delegates
+    
+    
+    // MARK: - User Actions
+    
+    func userLongPressed() {
+        print("User long pressed!!")
     }
     
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -39,6 +48,5 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
 
 }
